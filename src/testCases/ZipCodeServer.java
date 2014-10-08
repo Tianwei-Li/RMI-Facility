b5db02a52419479479131169e60b61183ec06efb
wendi <zwd199032@hotmail.com>
2014-10-08 17:07:17 -0400

    delete unused code.

--- src/testCases/ZipCodeServer.java
+++
@@ -1,9 +0,0 @@
-package testCases;
-public interface ZipCodeServer // extends YourRemote or whatever
-{
-    public void initialise(ZipCodeList newlist);
-    public String find(String city);
-    public ZipCodeList findAll();
-    public void printAll();
-}
-
